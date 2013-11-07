095df8559b6f01d5f638083b31e46d2460bd3805
Kevin Johnson <vadskye@gmail.com>
2013-11-07 13:41:01 -0800

    Basic image importing

---
+++ vis_test.py
@@ -0,0 +1,17 @@
+import numpy as np
+import scipy
+import pylab
+import pymorph
+import mahotas
+from scipy import ndimage
+
+chart = mahotas.imread('sample_chart.png');
+dna = mahotas.imread('dna.jpeg');
+
+print chart.shape
+print chart.dtype
+print chart.max()
+print chart.min()
+
+pylab.imshow(chart)
+pylab.show()
