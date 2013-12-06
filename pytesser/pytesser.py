63a0ddbd2db7a6ae28290a7b69d6f5bcfc14d1c1
Kevin Johnson <vadskye@gmail.com>
2013-12-06 15:41:09 -0800

    Suppress unnecessary "Tesseract" message when OCR

--- pytesser/pytesser.py
+++ pytesser/pytesser.py
@@ -8,17 +8,20 @@ import subprocess
 
 import util
 import errors
+import os
 
 tesseract_exe_name = 'tesseract' # Name of executable to be called at command line
 scratch_image_name = "temp.bmp" # This file must be .bmp or other Tesseract-compatible format
 scratch_text_name_root = "temp" # Leave out the .txt extension
 cleanup_scratch_flag = True  # Temporary files cleaned up after OCR operation
 
+#output suppressed as per https://code.google.com/p/pytesser/wiki/README
 def call_tesseract(input_filename, output_filename):
 	"""Calls external tesseract.exe on input file (restrictions on types),
 	outputting output_filename+'txt'"""
+	devnull = open(os.devnull, 'w') #modified
 	args = [tesseract_exe_name, input_filename, output_filename]
-	proc = subprocess.Popen(args)
+	proc = subprocess.Popen(args, stderr=devnull) #modified
 	retcode = proc.wait()
 	if retcode!=0:
 		errors.check_for_errors()
