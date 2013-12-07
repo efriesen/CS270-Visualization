6b6794d9ed33a19fdf94b5bbd793228654c3f521
David Huang <explorer.dsh@gmail.com>
2013-12-06 16:19:00 -0800

    Testing

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     args = initialize_argument_parser()
     input_file = args["input_file"]
     image=util.imread(input_file)
-    print image
+    #util.display_graph(image)
     image_labels, feature_count = identify.identify_features(image)
     print 'feature_count:', feature_count
     feature_types = identify.identify_feature_types(image, image_labels, feature_count)
